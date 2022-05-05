bee040baaf9acc4b46d7f3fb3074e8bbe0125fa5
MADBUG <Giglel94@gmail.com>
2022-05-05 10:34:03 +0300

    Prelimenary keybindings

---
+++ wog_advanced_artillery/CfgUserActions.hpp
@@ -0,0 +1,165 @@
+class CfgUserActions
+{
+  class WOG_D30_gun_elevation_up
+  {
+    displayName = "Gun elevation up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_elevation_down
+  {
+    displayName = "Gun elevation down";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_traverse_left
+  {
+    displayName = "Gun traverse left";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_traverse_right
+  {
+    displayName = "Gun traverse right";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_elevation_up
+  {
+    displayName = "Sight elevation up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_elevation_down
+  {
+    displayName = "Sight elevation down";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_traverse_left
+  {
+    displayName = "Sight traverse left";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_traverse_right
+  {
+    displayName = "Sight traverse right sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_elevation_up_multi
+  {
+    displayName = "Gun elevation up sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_elevation_down_multi
+  {
+    displayName = "Gun elevation down sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_traverse_left_multi
+  {
+    displayName = "Gun traverse left sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_gun_traverse_right_multi
+  {
+    displayName = "Gun traverse right sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_elevation_up_multi
+  {
+    displayName = "Sight elevation up sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_elevation_down_multi
+  {
+    displayName = "Sight elevation down sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_traverse_left_multi
+  {
+    displayName = "Sight traverse left sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_sight_traverse_right_multi
+  {
+    displayName = "Sight traverse right sped up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_corrector_elevation_up
+  {
+    displayName = "Corrector elevation up";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+  class WOG_D30_corrector_elevation_down
+  {
+    displayName = "Corrector elevation down";
+		tooltip = "This action is for testing.";
+		onActivate = "_this call TAG_fnc_MyHandler";		// _this is always true.
+		onDeactivate = "_this call TAG_fnc_MyHandler";		// _this is always false.
+		onAnalog = "_this call TAG_fnc_MyAnalogHandler";	// _this is the scalar analog value.
+		analogChangeThreshold = 0.1;
+  };
+};
